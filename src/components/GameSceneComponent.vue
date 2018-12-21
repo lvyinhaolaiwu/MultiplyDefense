d8162e8fd7933203b5342fabfb064f96f1bebaf5
blueheart <364232252@qq.com>
2018-12-21 12:57:45 +0800

    游戏简介和游戏框架设计

---
+++ src/components/GameSceneComponent.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+<canvas id="canvas" width="300" height="480">
+
+</canvas>
+  </div>
+</template>
+
+<script>
+export default {
+  // name: 'HelloWorld',
+  data () {
+    return {
+      // msg: 'Welcome to Your Vue.js App'
+      stage: null,
+      canvas: null,
+      // boxWidth: 50,
+      // boxHeight: 20,
+      // gameWidth: 300,
+      // gameHeight: 480,
+      // createjs: null,
+      // numberBoxes: []
+    }
+  },
+
+  mounted () {
+    this.canvas = document.getElementById('canvas')
+    this.stage = new window.createjs.Stage(this.canvas)
+    var hello = new window.createjs.Text('Hello CreateJS', '18px', 'white')
+    this.stage.addChild(hello)
+    this.stage.update()
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+#canvas{
+  background: #333;
+}
+</style>
---
+++ src/components/gamecontainer.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <header>
+      <div class="row">
+        <h1>Multiply Defense</h1>
+      </div>
+    </header>
+    <!--
+    <img src="./assets/logo.png">
+    <router-view/>
+    -->
+    <section id="game" class="row">
+<game-scene></game-scene>
+    </section>
+  </div>
+</template>
+
+<script>
+import GameScene from './GameSceneComponent'
+export default {
+  // name: 'App'
+  components: {
+    GameScene
+  }
+}
+</script>
+
+<style>
+  /*
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+*/
+  #game {
+    position: relative;
+    width: 300px;
+    height: 480px;
+    border: 1px solid black;
+  }
+</style>
